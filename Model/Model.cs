78e97be6430e1fa2b23563ead044b7b3cdfa39a0
Dawid Majewski <jdev.dm98@gmail.com>
2022-06-24 11:05:23 +0200

    first

---
+++ Model/Model.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MiniTC.Model
+{
+    class Model
+    {
+        public string[] GetAllDrives() => Directory.GetLogicalDrives();
+        public static string GetParentOfFile(string path) => Directory.GetParent(path).FullName;
+        public string[] GetAllFiles(string path)
+        {
+            List<string> Content = new List<string>();
+            try
+            {
+                if (Directory.GetParent(path) != null)
+                    Content.Add("..");
+
+                string[] Directories = Directory.GetDirectories(path);
+                string[] Files = Directory.GetFiles(path);
+
+                for (int i=0; i<Directories.Length; i++)
+                    Content.Add("<D>" + Path.GetFileName(Directories[i]));
+
+                for (int j = 0; j < Files.Length; j++)
+                    Content.Add(Path.GetFileName(Files[j]));
+            }
+            catch {}
+            return Content.ToArray();
+        }
+        public string ChangePath(string path, string selectedFile)
+        {
+            if (selectedFile != null && selectedFile != "..." && selectedFile.Contains("<D>") )
+            {
+                selectedFile = selectedFile.Replace("<D>", "");
+                string newPath = Path.Combine(path, selectedFile);
+                return newPath;
+            }
+
+            if (selectedFile == "...")
+                path = GetParentOfFile(path);
+
+            return path;
+        }
+        public void CopyFile(string source, string destination)
+        {
+            try
+            {
+                File.Copy(source, destination, true);
+            }
+            catch {}
+        }
+    }
+}
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,113 @@
+﻿using MiniTC.ViewModel.BaseClass;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using MiniTC.Annotations;
+
+namespace MiniTC.ViewModel
+{
+    using Model;
+    class ViewModel : INotifyPropertyChanged
+    {
+        Model _model = new Model();
+        
+
+        private string leftdirectory = null;
+        public string Left
+        {
+            get { return leftdirectory; }
+            set
+            {
+                leftdirectory = value;
+                OnPropertyChanged(nameof(CurrentLeftFiles));
+                OnPropertyChanged(nameof(Left));
+            }
+        }
+        public ObservableCollection<string> CurrentLeftFiles
+        {
+            get{ return new ObservableCollection<string> (_model.GetAllFiles(Left));}
+        }
+        private ICommand leftchange = null;
+        public ICommand LeftChceck
+        {
+            get
+            {
+                if (leftchange == null)
+                    leftchange = new RelayCommand(
+                        x => {Left = _model.ChangePath(Left, SelectedFile); },
+                        x => true);
+                return leftchange;
+            }
+        }
+        
+
+        private string rightdirectory = null;
+        public string Right
+        {
+            get { return rightdirectory; }
+            set
+            {
+                rightdirectory = value;
+                OnPropertyChanged(nameof(CurrentRightFiles));
+                OnPropertyChanged(nameof(Right));
+            }
+        }
+        public ObservableCollection<string> CurrentRightFiles
+        {
+            get{ return new ObservableCollection<string> (_model.GetAllFiles(Right)); }
+        }
+        private ICommand rightcheck = null;
+        public ICommand RightCheck
+        {
+            get
+            {
+                if (rightcheck == null)
+                    rightcheck = new RelayCommand(
+                    (arg) => { Right = _model.ChangePath(Right, SelectedFile);},
+                    (arg) => true );
+                return rightcheck;
+            }
+
+        }
+        
+
+        public ObservableCollection<string> CurrentDrives
+        {
+            get{ return new ObservableCollection<string>(_model.GetAllDrives()); }
+        }
+        public string SelectedFile { get; set; }
+        
+        private ICommand _copy = null;
+        public ICommand Copy
+        {
+            get
+            {
+                if (_copy == null)
+                {
+                    _copy = new RelayCommand(x =>
+                    {
+                        if (Right != null)
+                        {
+                            string source = Left + @"\" + SelectedFile;
+                            string dest = rightdirectory + @"\" + SelectedFile;
+                            _model.CopyFile(source, dest);
+                        }
+                        OnPropertyChanged(nameof(CurrentRightFiles));
+                    },
+                    x => true);
+                }
+                return _copy;
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
